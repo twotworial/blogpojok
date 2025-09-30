0c911fd235432d88b7769b2119bbbf0a9d51bfc7
Twotworial <reddamaskus@gmail.com>
2025-09-30 17:52:16 +0700

    Create config.ts

---
+++ src/content/config.ts
@@ -0,0 +1,29 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blogs = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/blogs" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string().optional(),
+      pubDate: z.coerce.date(),
+      updatedAt: z.coerce.date().optional(),
+      author: z.string().default("Twotworial"),
+      tags: z.array(z.string()).default([]),
+      draft: z.boolean().default(false),
+      // ⬇︎ penting: url boleh image() ATAU string
+      image: z
+        .union([
+          image(),
+          z.object({
+            url: z.union([image(), z.string()]),
+            alt: z.string().optional(),
+          }),
+          z.string(),
+        ])
+        .optional(),
+    }),
+});
+
+export const collections = { blogs };
