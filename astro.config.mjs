f6853a50b8c11a57c70b9ebf67f3cd5341547100
Twotworial <reddamaskus@gmail.com>
2025-10-01 08:39:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,13 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
-import swup from "@swup/astro";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://twotworial.com",
-  // Hapus ini karena deploy di root:
-  // base: "/space-ahead",
+  // base: "/space-ahead", // tetap nonaktif (deploy di root)
 
   integrations: [
-    swup({
-      theme: ["overlay", { direction: "to-top" }],
-      cache: true,
-      progress: true,
-    }),
     preact(),
     sitemap(),
   ],
